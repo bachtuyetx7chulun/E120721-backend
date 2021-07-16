ac6c3ca71064d8ec19d73a0701a1010e4c862eaf
DESKTOP-3FRC7CU\FABBI <fabbi@fabbi.io>
2021-07-16 11:47:15 +0700

    make it bad

--- app/bull/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./bull.init");
-console.log("Queue service is running");
--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,5 +1,6 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-export const PORT = process.env.HOST_PORT; 
-export const HOST_NAME = process.env.HOST_NAME; 
+export const PORT = process.env.HOST_PORT || 4000;
+export const HOST_NAME =
+    process.env.HOST_NAME || 'https://e120721.herokuapp.com/';
