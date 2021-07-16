a1737e07f90edd119d12715d6958557d93195890
bachtuyetx7chulun <contact.truongtrunghieu@gmail.com>
2021-07-16 21:30:32 +0700

    update

--- src/services/crawl.service.ts
+++ src/services/crawl.service.ts
@@ -2,6 +2,10 @@ import nodeSchedule from 'node-schedule';
 import axios from 'axios';
 
 // TODO: Crawl website per 30 minutes and save to realtime-db
-const jobs = nodeSchedule.scheduleJob('00 */10 * * * *', () => {
+const jobs1 = nodeSchedule.scheduleJob('00 */10 * * * *', () => {
     axios.get('https://e120721.herokuapp.com/api/v1/crawl');
+});
+
+const job2 = nodeSchedule.scheduleJob('00 */15 * * * *', () => {
+    axios.get('https://e120721.herokuapp.com/api/v1/detail');
 });
